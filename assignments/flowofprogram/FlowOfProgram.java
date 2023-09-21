5f73444ece51d96cf35a3b59505ca31b6d85c51b
kishorkumarg <kishorkumar.g@youngsoft.in>
2023-09-21 19:29:07 +0530

    completed flow of program assigments

--- assignments/flowofprogram/FlowOfProgram.java
+++ assignments/flowofprogram/FlowOfProgram.java
@@ -1,5 +1,7 @@
 package flowofprogram;
 
+import java.util.Scanner;
+
 public class FlowOfProgram {
 
     public static void main(String[] args) {
@@ -8,6 +10,17 @@ public class FlowOfProgram {
         System.out.println(isLeapYear(2000));
         System.out.println(isLeapYear(2022));
         System.out.println(isLeapYear(100));
+
+        multiplicationTableOfNumber(9);
+
+        System.out.println(LCM(5, 7));
+        System.out.println(LCM(10, 5));
+
+        System.out.println(GCD(5, 7));
+        System.out.println(GCD(10, 5));
+        System.out.println(GCD(12, 3));
+
+        System.out.println(keepOnTakingNumbersReturnSumUntilEntersX());
     }
 
     /**
@@ -18,5 +31,54 @@ public class FlowOfProgram {
      */
     public static boolean isLeapYear(int year) {
         return (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+    }
+
+    public static int sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void multiplicationTableOfNumber(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+
+    public static int LCM(int a, int b) {
+        int lcm = Math.max(a, b);
+        while (true) {
+            if (lcm % a == 0 && lcm % b == 0) {
+                return lcm;
+            }
+            lcm++;
+        }
+    }
+
+    public static int GCD(int a, int b) {
+        int gcd = Math.min(a, b);
+        while (gcd >= 1) {
+            if (a % gcd == 0 && b % gcd == 0) {
+                return gcd;
+            }
+            gcd--;
+        }
+        return gcd;
+    }
+
+    public static int keepOnTakingNumbersReturnSumUntilEntersX() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            System.out.print("Enter 'x' to quit, number to add :");
+            String num = scanner.next();
+            if (num.equalsIgnoreCase("x")) {
+                return sum;
+            }
+            try {
+                sum = sum + Integer.parseInt(num);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter x to quit, number to add..!!!");
+            }
+
+        }
     }
 }
